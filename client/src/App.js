98fba3ad63dc4edeb3220c5bf6197733537224ef
Ethan Nguyen <ethan@timebird.org>
2021-11-08 11:22:24 +0700

    Update client app

--- client/src/App.js
+++ client/src/App.js
@@ -9,8 +9,8 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">Image haithanh86/multi-client</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
